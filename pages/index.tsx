3bd4eaf3eed9b4082f00980cdbb74c4426b8aacb
Muditha Madusith <108977790+muditha-madusith@users.noreply.github.com>
2024-09-24 20:19:54 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
           />
           <ol>
             <li>
-              Get started by editing <code>pages/index.tsx</code>.
+              Get started by editing.
             </li>
             <li>Save and see your changes instantly.</li>
           </ol>
